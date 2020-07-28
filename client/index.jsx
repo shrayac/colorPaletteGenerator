8295a31f3a953b0624fc339159995b29596641a2
Shraya Chakma <shrayachakma@gmail.com>
2020-07-27 18:09:17 -0700

    Commit version without comments

--- client/index.jsx
+++ client/index.jsx
@@ -16,39 +16,25 @@ class App extends React.Component {
     // * Maybe something to look at later!
   }
   create () {
-    // * We want a color hex number
-    // * A 6 digit hexadecimal number
     return '#' +  Math.random().toString(16).substr(-6);
-    // * toString(16) is a hexadecimal
-    // * substring to get the last 6 for the color hex
   }
   changeNumber() {
-    // * Gets the value from the drop down!
     var value = document.getElementById('quantity').value
-    // * Makes it a number value
     var num = parseInt(value)
-    // * Creates an array for the new colors
     var updatedColors = [];
-    // * Based on the selected number - push it to the new array
     for (var i = 0; i < num; i++) {
       updatedColors.push({colorCode: this.create()});
     }
-    // * Set the state to the new colors :)
     this.setState({
       numOfColors: num,
-      // * num is what is chosen from the drop down
       colors: updatedColors,
-      // * sets it to the new updatedColors
     })
   }
   changeColor (i) {
-    // * Change colors individually
     var colors = this.state.colors.slice();
     colors[i].colorCode = this.create();
-    // * [i] is selected color
     this.setState({
       colors : colors
-      //* set it to the new color
     });
   }
 
